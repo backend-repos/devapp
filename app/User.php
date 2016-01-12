32021dd3562ef37352e8ba1a015634475f387fff
Shawn Sandy <shawnsandy04@gmail.com>
2016-01-12 08:36:47 -0500

    stories skeleton

--- app/User.php
+++ app/User.php
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function story(){
+        return $this->hasMany(Story::class);
+    }
 }
